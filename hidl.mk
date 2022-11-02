061ae4a29a3e22658d0addaa4375d39d3af6f0a6
Gabriel2392 <gabriel824m@gmail.com>
2022-11-02 00:28:17 +0000

    Nuke thermal hal

--- hidl.mk
+++ hidl.mk
@@ -13,11 +13,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.broadcastradio@1.0-impl
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.1-impl \
-    android.hardware.thermal@1.1-service
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
